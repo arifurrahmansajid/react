66521d3048fad717b5b025ec670e68b0e045bb47
arifurrahmansajid <iamnobody3456@gmail.com>
2025-04-05 22:36:36 +0600

    first commit

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect , useState } from 'react';
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() =>{
+        fetch("/public/blogs.json")
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+    console.log(blogs)
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Blogs;
